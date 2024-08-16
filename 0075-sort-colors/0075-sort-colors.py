f29375d95667bec29f139dc38a0436bca36ed1ef
Jossantos <155446223+josantoss@users.noreply.github.com>
2024-08-16 12:40:31 +0300

    Time: 43 ms (18.41%), Space: 16.3 MB (92.54%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,11 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        for _ in range(len(nums)):
-            i = 0
-            while i < len(nums)-1:
-                if nums[i] > nums[i+1]:
-                    nums[i],nums[i+1] = nums[i+1], nums[i]
-                i += 1
-        return nums
+        count = {}
+        for element in range(len(nums)):
+            count[nums[element]] = count.get(nums[element], 0) + 1
+        index = 0
+        for color in range(3):
+            value = count.get(color,0)
+            nums[index: index + value] = [color]*value
+            index += value
