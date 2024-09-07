e04a73902bf2ea2207382c29c6528111a733983e
Jossantos <155446223+josantoss@users.noreply.github.com>
2024-09-07 23:05:24 +0300

    Time: 414 ms (28.54%), Space: 22.4 MB (70.04%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -4,11 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums = set(nums)
-        i = 1
-        while True:
-            if i not in nums:
-                return i
-            i += 1
+        n = len(nums)
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[i] != nums[nums[i] - 1]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        return n + 1
         
         
